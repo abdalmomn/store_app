14564733cdec8e520e693aa81da74c3a2235813f
abdalmomn <113206277+abdalmomn@users.noreply.github.com>
2024-09-21 19:28:12 +0300

    delete secret key

--- store project/store_app/config/services.php
+++ store project/store_app/config/services.php
@@ -32,9 +32,9 @@ return [
     ],
 
     'google' => [
-        'client_id' => '38885443551-th6paeto72q1r5rutoa3p27fd0sr9ki2.apps.googleusercontent.com',
-        'client_secret' => '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',
-        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
     ],
 
     'apple' => [
