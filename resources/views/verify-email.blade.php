941736d8fefb63589f48414b914d10327e14111c
omarzenalden <154792860+omarzenalden@users.noreply.github.com>
2024-11-25 13:09:03 +0300

    Delete resources directory

--- resources/views/verify-email.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <div class="alert alert-success">
-            <p>A verification link has been sent to your email address. Please check your inbox.</p>
-        </div>
-    </div>
-@endsection
